fa0199d9a43c0a059c80a58a98ab8bbb4278cd23
Viacheslav417865 <sfinks417865@gmail.com>
2024-11-27 22:24:33 +0200

    Fix duplicate URL patterns in urls.py

--- movie_api/cinema/urls.py
+++ movie_api/cinema/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path("movies/", views.movie_list, name="movie-list"),
-    path("movies/<int:pk>/", views.movie_detail, name="movie-detail"),
     path("movies/", views.movie_create, name="movie-create"),
+
+    # Retrieve, update, or delete a single movie by ID
+    path("movies/<int:pk>/", views.movie_detail, name="movie-detail"),
     path("movies/<int:pk>/", views.movie_update, name="movie-update"),
     path("movies/<int:pk>/", views.movie_delete, name="movie-delete"),
 ]
